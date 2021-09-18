2c1b8fdc743e108702738113434fb07284d103f0
Ruben Montero <rubenmontero@rocketmail.com>
2021-09-18 14:12:15 +0200

    Router, checkout, adress form and stripe

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,32 +1,59 @@
-import React from 'react';
-import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core';
-import { CallMissedSharp, ShoppingCart } from '@material-ui/icons';
+import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Badge,
+  Typography,
+} from "@material-ui/core";
+import { ShoppingCart } from "@material-ui/icons";
+import { Link, useLocation } from "react-router-dom";
 
-import logo from '../../assets/commerce.png';
-import useStyles from './styles';
+import logo from "../../assets/commerce.png";
+import useStyles from "./styles";
 
-const Navbar = () => {
-    const classes = useStyles();
+const Navbar = ({ totalItems }) => {
+  const classes = useStyles();
+  const location = useLocation();
 
-    return (
-        <>
-            <AppBar position="fixed" className={classes.appBar} color="inherit">
-                <Toolbar>
-                    <Typography variant="h6" className={classes.title} color="inherit">
-                        <img src={logo} alt="Commerce.js" height="25px" className={classes.image} />
-                            Comerce.js     
-                    </Typography>  
-                    <div className={classes.grow} />
-                    <div className={classes.button} />
-                        <IconButton aria-label="Show cart items" color="inherit">
-                            <Badge badgeContent={2} color="secondary">
-                                <ShoppingCart />    
-                            </Badge>    
-                        </IconButton> 
-                </Toolbar>    
-            </AppBar>   
-        </>
-    )
-}
+  return (
+    <>
+      <AppBar position="fixed" className={classes.appBar} color="inherit">
+        <Toolbar>
+          <Typography
+            component={Link}
+            to="/"
+            variant="h6"
+            className={classes.title}
+            color="inherit"
+          >
+            <img
+              src={logo}
+              alt="Commerce.js"
+              height="25px"
+              className={classes.image}
+            />
+            Commerce.js
+          </Typography>
+          <div className={classes.grow} />
+          {location.pathname === "/" && (
+            <div className={classes.button}>
+              <IconButton
+                component={Link}
+                to="/cart"
+                aria-label="Show cart items"
+                color="inherit"
+              >
+                <Badge badgeContent={totalItems} color="secondary">
+                  <ShoppingCart />
+                </Badge>
+              </IconButton>
+            </div>
+          )}
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,18 +2,18 @@ import React from "react";
 import { Grid } from "@material-ui/core";
 
 import Product from "./Product/Product";
-import useStyles from './styles'
+import useStyles from "./styles";
 
-const Products = ({ products }) => {
-    const classes = useStyles();
+const Products = ({ products, onAddToCart }) => {
+  const classes = useStyles();
 
   return (
     <main className={classes.content}>
-        <div className={classes.toolbar} />
-      <Grid container justify="center" spacing={4}>
+      <div className={classes.toolbar} />
+      <Grid container justifyContent="center" spacing={4}>
         {products.map((product) => (
           <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-            <Product product={product} />
+            <Product product={product} onAddToCart={onAddToCart} />
           </Grid>
         ))}
       </Grid>
